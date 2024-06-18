157edcec18bd604fdac55969fe8c27d35497c4a9
younissk <62797468+younissk@users.noreply.github.com>
2024-06-19 00:38:50 +0200

    just changed the order

--- data/tests.ts
+++ data/tests.ts
@@ -4,8 +4,8 @@ import { python2_2023, python2_retry2023, python2_2021, python2_2022 } from "./e
 const tests: Map<string, Test> = new Map([
   ["python2-2023", python2_2023],
   ["python2-retry-2023", python2_retry2023],
-  ["python2-2021", python2_2021],
   ["python2-2022", python2_2022],
+  ["python2-2021", python2_2021],
 ]);
 
 export default tests;
