55ea9bcda498093bfe7ab51ea0243880281dab46
younissk <62797468+younissk@users.noreply.github.com>
2024-06-19 13:37:25 +0200

    add last exam

--- data/exams/index.ts
+++ data/exams/index.ts
@@ -1,5 +1,8 @@
 
+
+// Python 2
 export { default as python2_2023 } from "./python2/python2-2023";
 export { default as python2_retry2023 } from "./python2/python2_retry2023";
 export { default as python2_2022 } from "./python2/python2_2022";
 export { default as python2_2021 } from "./python2/python2_2021";
+export { default as python2_2024 } from "./python2/python2-2024";
--- data/tests.ts
+++ data/tests.ts
@@ -1,7 +1,8 @@
 import Test from "./interfaces/Test";
-import { python2_2023, python2_retry2023, python2_2021, python2_2022 } from "./exams";
+import { python2_2023, python2_retry2023, python2_2021, python2_2022, python2_2024 } from "./exams";
 
 const tests: Map<string, Test> = new Map([
+  ["python2_2024", python2_2024],
   ["python2-2023", python2_2023],
   ["python2-retry-2023", python2_retry2023],
   ["python2-2022", python2_2022],
