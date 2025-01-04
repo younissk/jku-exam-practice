0b11306ee4b29469eaf180eebeb7819d82c95bb5
kandah@abk.at <62797468+younissk@users.noreply.github.com>
2025-01-04 14:57:45 +0100

    Add ability to add pictures to the questions

--- src/components/QuestionHTMLEditor.tsx
+++ src/components/QuestionHTMLEditor.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
@@ -9,12 +9,52 @@ interface QuestionHTMLEditorProps {
 
 const QuestionHTMLEditor: React.FC<QuestionHTMLEditorProps> = ({ content, setContent }) => {
 
+  const handleImageUpload = useCallback(() => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = async () => {
+      const file = input.files ? input.files[0] : null;
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          const range = quillRef.current?.getEditor().getSelection();
+          if (range) {
+            quillRef.current?.getEditor().insertEmbed(range.index, 'image', reader.result);
+          }
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+  }, []);
+
+  const modules = {
+    toolbar: {
+      container: [
+        [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+        [{size: []}],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+        ['link', 'image'],
+        ['clean']
+      ],
+      handlers: {
+        image: handleImageUpload
+      }
+    }
+  };
+
+  const quillRef = React.useRef<ReactQuill>(null);
 
   return (
     <ReactQuill
+      ref={quillRef}
       value={content}
       onChange={setContent}
       theme="snow"
+      modules={modules}
     />
   );
 };
